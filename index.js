7cc48a8ad7d130b2cf82de61484ff24922d1d130
chattie <christuaik@gmail.com>
2023-02-12 15:04:16 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    return [...cats, name];
+}
+
+function prependCat(name){
+    return [name, ...cats];
+}
+
+function removeLastCat(){
+    const cats2 = cats.slice();
+    cats2.pop();
+    return cats2;
+}
+
+function removeFirstCat(){
+    const cats2 = cats.slice();
+    cats2.shift();
+    return cats2;
+}
